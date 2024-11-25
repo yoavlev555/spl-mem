37bf09a0ae04431e0dc3550d20d4d6704bc3e001
Yoav Levin <yoavlev555@gmail.com>
2024-11-25 08:19:27 +0000

    update v1.2

--- include/Simulation.h
+++ include/Simulation.h
@@ -16,7 +16,7 @@ class Simulation {
         void start();
         void addPlan(const Settlement &settlement, SelectionPolicy *selectionPolicy);
         void addAction(BaseAction *action);
-        bool addSettlement(Settlement settlement);
+        bool addSettlement(Settlement *settlement);
         bool addFacility(FacilityType facility);
         bool isSettlementExists(const string &settlementName);
         Settlement &getSettlement(const string &settlementName);
@@ -30,6 +30,6 @@ class Simulation {
         int planCounter; //For assigning unique plan IDs
         vector<BaseAction*> actionsLog;
         vector<Plan> plans;
-        vector<Settlement> settlements;
+        vector<Settlement*> settlements;
         vector<FacilityType> facilitiesOptions;
 };
