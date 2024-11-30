5e5c12ca829114bf93ba60eec299e307f16379e2
Yoav Levin <yoavlev555@gmail.com>
2024-11-30 22:55:15 +0000

    Start simulation implementation

--- src/Simulation.cpp
+++ src/Simulation.cpp
@@ -1,12 +1,90 @@
 #include <iostream>
 #include "Simulation.h"
-
+#include "Auxiliary.h"
+#include <fstream> 
 using namespace std;
 
-Simulation::Simulation(const string& config){
-    this->isRunning = true;
-    this->planCounter = 0;
+Simulation::Simulation(const string &configFilePath):isRunning(true), planCounter(0),actionsLog(vector<BaseAction*>()), plans(vector<Plan>()), settlements(vector<Settlement*>()), facilitiesOptions(vector<FacilityType>()){
+    string line;
+    ifstream MyReadFile(configFilePath);
+    while (getline (MyReadFile, line)){
+        vector<string> lineArguments = Auxiliary::parseArguments(line);
+        if (lineArguments.at(0) == "settlement"){
+            string st = lineArguments.at(2);
+            SettlementType type = SettlementType::CITY;
+
+            if(st == "0"){
+                type = SettlementType::VILLAGE;
+            }
+
+            else if(st == "1"){
+                type = SettlementType::CITY;
+            }
+
+            else{
+                type = SettlementType::METROPOLIS;
+            }
+            
+            settlements.push_back(new Settlement(lineArguments.at(1),type));
+        }
+
+        else if (lineArguments.at(0) == "facility"){
+            string fc = lineArguments.at(2);
+            FacilityCategory category = FacilityCategory::ECONOMY;
+
+            if(fc == "0"){
+                category = FacilityCategory::LIFE_QUALITY;
+            }
+
+            else if(fc == "1"){
+                category = FacilityCategory::ECONOMY;
+            }
+
+            else{
+                category = FacilityCategory::ENVIRONMENT;
+            }
+
+            facilitiesOptions.push_back(FacilityType(lineArguments.at(1),category,stoi(lineArguments.at(3)),stoi(lineArguments.at(4)),stoi(lineArguments.at(5)),stoi(lineArguments.at(6))));
+        }
+
+        else if (lineArguments.at(0) == "plan"){
+            Settlement* set;
+            for (int i = 0; i< settlements.size(); i++){
+                if(settlements.at(i)->getName() == lineArguments.at(1)){
+                    set = settlements.at(i);
+                    break;
+                }
+            }
+
+            if (lineArguments.at(2)=="nve"){
+                plans.push_back(Plan(planCounter,*set , new NaiveSelection(), facilitiesOptions));
+            }
+
+            else if (lineArguments.at(2)=="bal"){
+                plans.push_back(Plan(planCounter,*set , new BalancedSelection(0,0,0), facilitiesOptions));
+            }
+
+            else if (lineArguments.at(2)=="eco"){
+                plans.push_back(Plan(planCounter,*set , new EconomySelection(), facilitiesOptions));
+            }
+
+            else if (lineArguments.at(2)=="env"){
+                plans.push_back(Plan(planCounter,*set , new SustainabilitySelection(), facilitiesOptions));
+            }
+
+            planCounter++;
+        }
+    }
+    //# plan <settlement_name> <selection_policy>
 }
+/*
+bool isRunning;
+        int planCounter; //For assigning unique plan IDs
+        vector<BaseAction*> actionsLog;
+        vector<Plan> plans;
+        vector<Settlement*> settlements;
+        vector<FacilityType> facilitiesOptions;
+        */
 
 
 void Simulation::start(){
@@ -15,3 +93,31 @@ void Simulation::start(){
 
     }
 }
+
+
+
+
+
+/*
+class Simulation {
+    public:
+        void start();
+        void addPlan(const Settlement &settlement, SelectionPolicy *selectionPolicy);
+        void addAction(BaseAction *action);
+        bool addSettlement(Settlement *settlement);
+        bool addFacility(FacilityType facility);
+        bool isSettlementExists(const string &settlementName);
+        Settlement &getSettlement(const string &settlementName);
+        Plan &getPlan(const int planID);
+        void step();
+        void close();
+        void open();
+
+    private:
+        bool isRunning;
+        int planCounter; //For assigning unique plan IDs
+        vector<BaseAction*> actionsLog;
+        vector<Plan> plans;
+        vector<Settlement*> settlements;
+        vector<FacilityType> facilitiesOptions;
+};*/
--- src/main.cpp
+++ src/main.cpp
@@ -12,56 +12,8 @@ int main(int argc, char** argv){
         cout << "usage: simulation <config_path>" << endl;
         return 0;
     }
-    // string configurationFile = argv[1];
-    // Simulation simulation(configurationFile);
-    // simulation.start();
-    // if(backup!=nullptr){
-    // 	delete backup;
-    // 	backup = nullptr;
-    // }
 
-    /*class BalancedSelection: public SelectionPolicy {
-    public:
-        BalancedSelection(int LifeQualityScore, int EconomyScore, int EnvironmentScore);
-        BalancedSelection(const BalancedSelection& other);
-        const FacilityType& selectFacility(const vector<FacilityType>& facilitiesOptions) override;
-        const string toString() const override;
-        BalancedSelection *clone() const override;
-        ~BalancedSelection() override = default;
-        int checkDiff(int lifeScore, int economyScore, int environmentScore);
-        BalancedSelection& operator=(const BalancedSelection& other);
-
-    private:
-        int LifeQualityScore;
-        int EconomyScore;
-        int EnvironmentScore;
-};
-*/
-
-
-
-
-    vector<FacilityType> ft = vector<FacilityType>();
-    ft.push_back(FacilityType("Desalination Plant",FacilityCategory::ECONOMY,2,2,1,2));
-    ft.push_back(FacilityType("Kindergarten",FacilityCategory::ECONOMY,12,0,2,0));
-    ft.push_back(FacilityType("Hospital",FacilityCategory::ENVIRONMENT,6,3,1,2));
-    ft.push_back(FacilityType("1",FacilityCategory::ECONOMY,12,1,0,0));
-    ft.push_back(FacilityType("2",FacilityCategory::ECONOMY,12,1,0,0));
-    ft.push_back(FacilityType("3",FacilityCategory::ENVIRONMENT,12,1,0,0));
-
-
-    Settlement s = Settlement("Facili", SettlementType::METROPOLIS);
-    
-
-    Plan test = Plan(16, s, new NaiveSelection(), ft);
-
-    for (int i=0; i<10; i++){
-        test.step();
-        test.toString();
-    }
-
-
-
+    Simulation* s = new Simulation(argv[1]);
 
     return 0;
 }
